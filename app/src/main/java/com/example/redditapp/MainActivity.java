ca88c5847e0d4b6b9bcc20aa598f20eed4d73e80
Sasha <Acount@i.ua>
2020-12-14 17:25:07 +0200

    Aave image to gallery

--- app/src/main/java/com/example/redditapp/MainActivity.java
+++ app/src/main/java/com/example/redditapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.redditapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
@@ -9,11 +12,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    //private ImageView mImageView;
     private NewsAdapter mAdapter;
     private static final String USGS_REQUEST_URL = "https://www.reddit.com/top.json";
 
@@ -21,6 +27,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActivityCompat.requestPermissions(MainActivity.this ,
+                new String[]{
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1
+        );
+
+        ActivityCompat.requestPermissions(MainActivity.this ,
+                new String[]{
+                        Manifest.permission.READ_EXTERNAL_STORAGE}, 1
+        );
 
         ListView listView = findViewById(R.id.mainList);
         mAdapter = new NewsAdapter(this, new ArrayList<TopNews>());
@@ -39,7 +55,42 @@ public class MainActivity extends AppCompatActivity {
 
         TopNewsAsyncTask task = new TopNewsAsyncTask();
         task.execute(USGS_REQUEST_URL);
+
+        //mImageView = listView.findViewById(R.id.imageNews);
+
+//        Button btnSaveToG = listView.findViewById(R.id.btnSave);
+//
+//        View.OnClickListener clickListener = new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                saveImageToGallery();
+//            }
+//        };
+//        btnSaveToG.setOnClickListener(clickListener);
     }
+
+//    private void saveImageToGallery() {
+//        //to get the image from the ImageView (say iv)
+//        BitmapDrawable draw = (BitmapDrawable) mImageView.getDrawable();
+//        Bitmap bitmap = draw.getBitmap();
+//
+//        FileOutputStream outStream = null;
+//        File sdCard = Environment.getExternalStorageDirectory();
+//        File dir = new File(sdCard.getAbsolutePath() + "/MyPics");
+//        dir.mkdirs();
+//        String fileName = String.format("%d.jpg", System.currentTimeMillis());
+//        File outFile = new File(dir, fileName);
+//        try {
+//            outStream = new FileOutputStream(outFile);
+//            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outStream);
+//            outStream.flush();
+//            outStream.close();
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
     private class TopNewsAsyncTask extends AsyncTask<String, Void, List<TopNews>> {
 
--- app/src/main/java/com/example/redditapp/NewsAdapter.java
+++ app/src/main/java/com/example/redditapp/NewsAdapter.java
@@ -1,31 +1,56 @@
 package com.example.redditapp;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 
 public class NewsAdapter extends ArrayAdapter<TopNews> {
+
+    ImageView mImageView;
+    View ListItemView;
 
     public NewsAdapter(@NonNull Context context, List<TopNews> topNewsObjects) {
         super(context, 0, topNewsObjects);
     }
+
+//    ActivityCompat.requestPermissions((Activity) getContext(),
+//                new String[]{
+//        Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1
+//            );
+//
+//        ActivityCompat.requestPermissions((Activity) getContext(),
+//                new String[]{
+//        Manifest.permission.READ_EXTERNAL_STORAGE}, 1
+//            );
 
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         //return super.getView(position, convertView, parent);
 
-        View ListItemView = convertView;
+        ListItemView = convertView;
         if (ListItemView == null) {
             ListItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item_news, parent, false);
         }
@@ -44,14 +69,48 @@ public class NewsAdapter extends ArrayAdapter<TopNews> {
         dateAddView.setText(formatedDate);
 
         //image
-        ImageView imageView = ListItemView.findViewById(R.id.imageNews);
-        ImageManager.fetchImage(currentTopNews.getThumbnailUrl(), imageView);
+        mImageView = ListItemView.findViewById(R.id.imageNews);
+        ImageManager.fetchImage(currentTopNews.getThumbnailUrl(), mImageView);
 
         //quantity comments
         TextView commentsView = (TextView) ListItemView.findViewById(R.id.comments);
         commentsView.setText(new Integer(currentTopNews.getComments()).toString());
 
+        Button btnSaveToG = ListItemView.findViewById(R.id.btnSave);
+
+        btnSaveToG.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                ImageView selectImageView = ListItemView.findViewById(R.id.imageNews);
+                saveImageToGallery(selectImageView);
+            }
+        });
         return ListItemView;
+    }
+
+    private void saveImageToGallery(ImageView pImageView) {
+
+        //to get the image from the ImageView (say iv)
+        BitmapDrawable draw = (BitmapDrawable) pImageView.getDrawable();
+        Bitmap bitmap = draw.getBitmap();
+
+        FileOutputStream outStream = null;
+        File sdCard = new File(Environment.getExternalStorageState());
+        File dir = new File(sdCard.getAbsolutePath()); // + "/MyPics"
+        dir.mkdirs();
+        String fileName = String.format("%d.jpg", System.currentTimeMillis());
+        File outFile = new File(dir, fileName);
+        try {
+            outStream = new FileOutputStream(outFile);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outStream);
+            outStream.flush();
+            outStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private String formatDate(Date dateObject) {
